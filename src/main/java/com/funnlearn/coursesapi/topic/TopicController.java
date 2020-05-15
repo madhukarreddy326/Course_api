1978af29bce7aa70a011a3b2575d6d92f9f3a4c6
Madhukar <madhukar.reddy326@gmail.com>
2020-05-15 23:24:46 +0530

    added getDetailofTopic and create topic services. @Pathvariable is used to get pathparameter and @RequestBody is used to fetch the request body.

--- src/main/java/com/funnlearn/coursesapi/topic/TopicController.java
+++ src/main/java/com/funnlearn/coursesapi/topic/TopicController.java
@@ -3,8 +3,13 @@ package com.funnlearn.coursesapi.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +22,19 @@ public class TopicController {
 	{
 		return topicService.getAllTopics();
 	}
-				
+			
+	@RequestMapping("/topic/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topic")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+	}
+	
 				
 		
 	
--- src/main/java/com/funnlearn/coursesapi/topic/TopicService.java
+++ src/main/java/com/funnlearn/coursesapi/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.funnlearn.coursesapi.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,16 +9,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topic =Arrays.asList(
+	private List<Topic> topic =new ArrayList<>(Arrays.asList(
 			new Topic("java","java","Java Topic"),
 			new Topic("spring","spring","spring descpription"),
 			new Topic("javascript","javascript","javascript descrption")
 			
-			);
+			));
 	
 	public List<Topic> getAllTopics()
 	{
 		return topic;
+	}
+	public Topic getTopic(String id)
+	{
+		return topic.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic)
+	{
+		this.topic.add(topic);
 	}
 
 }
